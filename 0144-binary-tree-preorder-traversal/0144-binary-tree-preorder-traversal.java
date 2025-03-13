fbad59a508d648c3fb820d01bf522cf4a36e302a
Dipak Das <dipakdas84630@gmail.com>
2025-03-13 10:57:49 +0530

    Time: 0 ms (100%), Space: 42.2 MB (8.32%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,14 +14,17 @@
  * }
  */
 class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> answer = new ArrayList<>();
+    public List<Integer> traverse(TreeNode root, List<Integer> answer) {
         if(root == null)
             return answer;
 
         answer.add(root.val);
-        answer.addAll(preorderTraversal(root.left));
-        answer.addAll(preorderTraversal(root.right));
+        traverse(root.left, answer);
+        traverse(root.right, answer);
         return answer;
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> answer = new ArrayList<>();
+        return traverse(root, answer);
     }
 }
