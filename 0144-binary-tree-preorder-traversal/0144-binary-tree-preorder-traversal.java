b4ee26e874b1dd9d0f5e7e4ac3e044e3a252ac1e
Dipak Das <dipakdas84630@gmail.com>
2025-03-14 18:35:27 +0530

    Time: 0 ms (100%), Space: 42.1 MB (8.33%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,43 +14,43 @@
  * }
  */
 // <---------- RECURSIVE ---------->
-//  class Solution {
-//     public void preorderHelper(TreeNode root, List<Integer> answer) {
-//         if (root == null)   
-//             return;
+ class Solution {
+    public void preorderHelper(TreeNode root, List<Integer> answer) {
+        if (root == null)   
+            return;
             
-//         answer.add(root.val);
-//         preorderHelper(root.left, answer);
-//         preorderHelper(root.right, answer);
-//     }
+        answer.add(root.val);
+        preorderHelper(root.left, answer);
+        preorderHelper(root.right, answer);
+    }
 
-//     public List<Integer> preorderTraversal(TreeNode root) {
-//         List<Integer> answer = new ArrayList<>();
-//         preorderHelper(root, answer);
-//         return answer;
-//     }
-// }
-
-// <---------- ITERATIVE ---------->
-class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> answer = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-
-        if(root == null)
-            return answer;
-
-        stack.push(root);
-        while(!stack.isEmpty()) {
-            TreeNode node = stack.pop();
-            answer.add(node.val);
-
-            if(node.right != null)
-                stack.push(node.right);
-            if(node.left != null)
-                stack.push(node.left);
-            
-        }
+        preorderHelper(root, answer);
         return answer;
     }
 }
+
+// <---------- ITERATIVE ---------->
+// class Solution {
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         List<Integer> answer = new ArrayList<>();
+//         Stack<TreeNode> stack = new Stack<>();
+
+//         if(root == null)
+//             return answer;
+
+//         stack.push(root);
+//         while(!stack.isEmpty()) {
+//             TreeNode node = stack.pop();
+//             answer.add(node.val);
+
+//             if(node.right != null)
+//                 stack.push(node.right);
+//             if(node.left != null)
+//                 stack.push(node.left);
+            
+//         }
+//         return answer;
+//     }
+// }
