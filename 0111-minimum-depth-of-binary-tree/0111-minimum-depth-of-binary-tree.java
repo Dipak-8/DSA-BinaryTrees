7f064a72aadaa1dd4c7f383c729f4c00a1d4329f
Dipak Das <dipakdas84630@gmail.com>
2025-07-02 23:49:31 +0530

    Time: 2 ms (86.84%), Space: 63.5 MB (36.81%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -14,20 +14,47 @@
  * }
  */
 class Solution {
+    // public int minDepth(TreeNode root) {
+    //     if(root == null)
+    //         return 0;
+    //     int[] depth = new int[1];
+    //     depth[0] = Integer.MAX_VALUE;
+    //     findMinDepth(root, depth, 1);
+    //     return depth[0];
+    // }
+    // private void findMinDepth(TreeNode node, int[] depth, int level) {
+    //     if(node == null) 
+    //         return;
+    //     if(node.left == null && node.right == null) 
+    //         depth[0] = Math.min(depth[0], level);
+    //     findMinDepth(node.left, depth, level+1);
+    //     findMinDepth(node.right, depth, level+1);
+    // }
     public int minDepth(TreeNode root) {
-        if(root == null)
-            return 0;
-        int[] depth = new int[1];
-        depth[0] = Integer.MAX_VALUE;
-        findMinDepth(root, depth, 1);
-        return depth[0];
-    }
-    private void findMinDepth(TreeNode node, int[] depth, int level) {
-        if(node == null) 
-            return;
-        if(node.left == null && node.right == null) 
-            depth[0] = Math.min(depth[0], level);
-        findMinDepth(node.left, depth, level+1);
-        findMinDepth(node.right, depth, level+1);
+        Queue<TreeNode> queue = new LinkedList<>();
+        int depth = 0;
+        boolean flag = false;
+        if (root == null)
+            return depth;
+
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int level = queue.size();
+
+            for (int i = 0; i < level; i++) {
+                TreeNode node = queue.poll(); 
+                
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+                if(node.left == null && node.right == null) 
+                    flag = true;
+            }
+            depth++;
+            if(flag)
+                break;
+        }
+        return depth;
     }
 }
